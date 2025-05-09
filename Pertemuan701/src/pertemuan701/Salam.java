92b590fc63be27e4b83eb727a13c645ba93b0cce
Sandi Yasa <160020184+SandiYasa@users.noreply.github.com>
2025-05-09 11:52:40 +0800

    Add files via upload

---
+++ Pertemuan701/src/pertemuan701/Salam.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pertemuan701;
+
+/**
+ *
+ * @author Sandi
+ * TGL 03 Mei 2015
+ */
+public class Salam {
+    private String tx="Tuliskan Pesan anda";
+    
+    public void tulisPesan(){
+        System.out.println(this.tx);
+    }
+    public void tulisPesan(String txt1){
+        System.out.println(txt1);
+    }
+    public void tulisPesan(Double txt2){
+        System.out.println(txt2);
+    }
+    
+}
